1f0db6f7cc4b5efb8a42b42089c362cf9b727b5a
Alex <35499736+alexhigley@users.noreply.github.com>
2021-12-01 17:23:04 -1000

    Update loop.py
    Added comments/ a description

--- getting_dat/loop.py
+++ getting_dat/loop.py
@@ -22,3 +22,14 @@ for i in range(len(dxyz)):
     path = '/home/ahigley/timing_practice'
     os.rename('new.par', os.path.join(path, 's' + stit[i] + '.par'))
     os.system('mv observatories.dat.bak observatories.dat')
+
+# my small, poorly named function that calls mobs from moveobsfun and performs most of the simulating of the data. 
+# dxyz represents adder from moveobsfun— that is, how much distance is being added onto the original coordinate for the observatory 
+# stit is just an array of strings to help with naming the simulated pulsar data that comes out the other end as 13 text files (one for each adder)
+# The loop does several things. First it calls mobs which moves the observatory. Then, it runs TEMPO2 which takes in the .par and .tim files for
+# the desired pulsar and produces the new .par file withe the predicted measurements. It will be different than the input .par file because the observatoy
+# position has changed. That being said, not much change happens at small differences in position. 
+# It renames the .par file with my naming system and then replaces the observatory file (which now has altred coordinates) with the original. 
+# This process is then repeated for each adder. 
+# This was the program that was ran with SLURM on bowser, some pulsars taking a few seconds and others taking days depending on how many TOAs (time of arrivals)
+# were in the .tim file. For example, pulsars that have been observed a lot took the most time 
